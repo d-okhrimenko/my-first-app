a10c6b2b64c2659085c47c9451305e93dec4df54
Dmitriy Okhrimenko <d.okhrimenko@cbsystematics.com>
2024-10-07 12:44:14 +0300

    test staged files

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -10,7 +10,7 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world"); //
             Console.ReadLine();
             // new line
         }
